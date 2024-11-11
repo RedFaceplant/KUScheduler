788931bb34cc13d5cbdd6326c7f0ea6e954b5373
nemograce <leonard.g.naomi@gmail.com>
2024-11-11 16:54:02 -0500

    Added a main screen and Profile menu

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+const profile = document.querySelector('#profile')
+const profileMenu = document.querySelector('#profileMenu')
+
+profile.addEventListener('click', () => {
+    profileMenu.classList.toggle('hide')
+})
+document.addEventListener('click', e => {
+    if( !profileMenu.contains(e.target) && e.target !== profile){
+        profileMenu.classList.add('hide')
+    }
+})

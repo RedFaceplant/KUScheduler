0e65304fe903f9538a51e3fe768db647799b1783
RedFaceplant <ashtonking321@gmail.com>
2024-12-09 10:38:49 -0500

    CSS is no longer absolute
    Credit to Naomi for the code, I only implemented it.
    Mostly Visual Changes.

--- dashboard.js
+++ dashboard.js
@@ -1,22 +1,13 @@
-// const profile = document.querySelector('#profile')
 
-// User bubble show/hide system:
-// profile.addEventListener('click', () => {
-//     profileMenu.classList.toggle('hide')
-// })
-
-// function showProfile(){
-//     const profileMenu = document.querySelector('#profileMenu')
-//     console.log(profileMenu.classList.toggle('hide'))
-// }
-// document.addEventListener('click', e => {
-//     const profileMenu = document.querySelector('#profileMenu')
-//     if( !profileMenu.contains(e.target) && e.target !== profile){
-//         profileMenu.classList.add('hide')
-//     }
-// })
-
-function logOut(){
-    currentUser = null
-    renderLogin()
+function dashboardInit(){
+const Profile = document.querySelector('#Profile')
+const profileMenu = document.querySelector('#profileMenu')
+    Profile.addEventListener('click', () => {
+        profileMenu.classList.toggle('hide')
+    })
+    document.addEventListener('click', e => {
+        if( !profileMenu.contains(e.target) && e.target !== Profile){
+            profileMenu.classList.add('hide')
+        }
+    })
 }
